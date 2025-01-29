8c4b5ab86a96f4a0158b7a11938943118fbfb5d0
ladislas GABRIEL <contact.lagabservices@gmail.com>
2025-01-29 15:25:06 +0100

    fix sidebar issues on mobile

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,7 +10,7 @@ import {cn} from "@/lib/utils"
 import {Button} from "@/components/ui/button"
 import {Input} from "@/components/ui/input"
 import {Separator} from "@/components/ui/separator"
-import {Sheet, SheetContent} from "@/components/ui/sheet"
+import {Sheet, SheetContent, SheetDescription, SheetTitle} from "@/components/ui/sheet"
 import {Skeleton} from "@/components/ui/skeleton"
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger,} from "@/components/ui/tooltip"
 
@@ -201,6 +201,8 @@ const Sidebar = React.forwardRef<
                         }
                         side={side}
                     >
+                        <SheetTitle/>
+                        <SheetDescription/>
                         <div className="flex h-full w-full flex-col">{children}</div>
                     </SheetContent>
                 </Sheet>
